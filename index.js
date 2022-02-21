a77ee32ca1443a035010c2612fb9b40b9a72ad02
Lea Larrama <leandrolarrama@hotmail.com>
2022-02-21 12:07:21 -0300

    haciendo una calculadora

---
+++ index.js
@@ -0,0 +1,11 @@
+const sumar = require("./sumar")
+// console.log(sumar(2,2))
+
+const restar = require("./restar")
+//console.log(restar(2,2))
+
+const multiplicar = require("./multiplicar")
+// console.log(multiplicar(2,2))
+
+const dividir = require("./dividir")
+//console.log(sumar(2,0))
